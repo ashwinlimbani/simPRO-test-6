a71f9cd141e2ecfa68296081ecf0050ff74c197d
ashwinlimbani <ashwinlimbani23@gmail.com>
2023-03-22 23:30:19 +0530

    get data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-test';
+  input = '2222';
+  data!: string;
+
+  constructor() {
+    this.data = this.getData(this.input);
+  }
+
+  /**
+   * This function should take the input and for each char
+   * increment if it's position in the string is even and
+   * decrement it if it's odd.
+   * This new value is then has its characters added up
+   * and this value is returned;
+   */
+  getData(input: string) {
+    if (isNaN(Number(input))) {
+      throw new Error('Invalid input');
+    }
+
+    const arr = input.split('');
+    let newVal = '';
+    for (let i = 0; i < arr.length; i++) {
+      if (i % 2 == 1) {
+        let num = Number(arr[i]);
+        newVal += ++num;
+      } else {
+        let num = Number(arr[i]);
+        newVal += --num;
+      }
+    }
+
+    return newVal;
+  }
 }
